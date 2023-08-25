401439a83dd53748000408ff2882a24998aedc64
ToletiSri <63068356+ToletiSri@users.noreply.github.com>
2023-08-25 16:16:19 +0530

    Update LightningModel.py

--- S13/LightningModel.py
+++ S13/LightningModel.py
@@ -149,8 +149,8 @@ class LitYolo(LightningModule):
     def configure_optimizers(self):
         
         
-        suggested_lr = self.lr_finder() #check on self.train_dataloader
-        #suggested_lr = 6.25E-03
+        #suggested_lr = self.lr_finder() #check on self.train_dataloader
+        suggested_lr = 7.25E-03
         
         steps_per_epoch = len(self.train_dataloader())
         scheduler_dict = {
