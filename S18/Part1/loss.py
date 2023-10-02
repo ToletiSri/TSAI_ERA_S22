3dd2b6ce38343c8248aff6d9e26c4e657c592e8b
ToletiSri <63068356+ToletiSri@users.noreply.github.com>
2023-10-02 21:46:01 +0530

    Add files via upload

--- S18/Part1/loss.py
+++ S18/Part1/loss.py
@@ -11,3 +11,46 @@ def dice_loss(pred, target):
     dice = (2. * intersection + smooth) / (union + smooth)
     
     return 1 - dice  
+
+
+import torch
+import torch.nn as nn
+
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, pred, target):
+        smooth = 1e-5
+        pred = pred.float()
+        target = target.float()
+        pred.requires_grad = True
+        target.requires_grad = True
+
+        # Define constants for classes
+        FOREGROUND = 1
+        BACKGROUND = 2
+        NOT_CLASSIFIED = 3
+
+        # Convert trimaps to binary masks for foreground
+        mask_pred_foreground = (pred == FOREGROUND).float()  # Binary mask: 1 for foreground, 0 otherwise
+        mask_target_foreground = (target == FOREGROUND).float()  # Binary mask: 1 for foreground, 0 otherwise
+
+        mask_pred_foreground.requires_grad = True
+        mask_target_foreground.requires_grad = True
+
+        # Calculate intersection and union for the foreground
+        intersection = torch.sum(mask_pred_foreground * mask_target_foreground, dim=(1, 2, 3))
+        union = torch.sum(mask_pred_foreground + mask_target_foreground, dim=(1, 2, 3))  # Union = A + B - Intersection
+
+        
+        # Flatten predictions and targets
+        # pred = pred.view(-1)
+        # target = target.view(-1)
+        
+        # intersection = (pred * target).sum()
+        # union = pred.sum() + target.sum()
+        
+        dice = (2. * intersection + smooth) / (union + smooth)
+        
+        return 1 - dice
