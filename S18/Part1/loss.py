afb0c4937b085c3f0866a16f0972e46fed902a80
ToletiSri <63068356+ToletiSri@users.noreply.github.com>
2023-10-05 19:25:10 +0530

    Update loss.py - try original implementation for log loss

--- S18/Part1/loss.py
+++ S18/Part1/loss.py
@@ -22,26 +22,11 @@ class DiceLoss(nn.Module):
 
     def forward(self, pred, target):
         smooth = 1e-5
-        pred = pred.float()
-        target = target.float()
-        pred.requires_grad = True
-        target.requires_grad = True
-
-        # Define constants for classes
-        FOREGROUND = 1
-        BACKGROUND = 2
-        NOT_CLASSIFIED = 3
-
-        # Convert trimaps to binary masks for foreground
-        mask_pred_foreground = (pred == FOREGROUND).float()  # Binary mask: 1 for foreground, 0 otherwise
-        mask_target_foreground = (target == FOREGROUND).float()  # Binary mask: 1 for foreground, 0 otherwise
-
-        mask_pred_foreground.requires_grad = True
-        mask_target_foreground.requires_grad = True
-
-        # Calculate intersection and union for the foreground
-        intersection = torch.sum(mask_pred_foreground * mask_target_foreground, dim=(1, 2, 3))
-        union = torch.sum(mask_pred_foreground + mask_target_foreground, dim=(1, 2, 3))  # Union = A + B - Intersection
+        target = torch.cat([ (target == i) for i in range(1,4) ], dim=1)
+        pred = pred.view(-1)
+        target = target.view(-1)
+        intersection = (pred * target).sum()
+        union = pred.sum() + target.sum()
         
         dice = (2. * intersection + smooth) / (union + smooth)
       
